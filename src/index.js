60ac4dd9af280f986c63573bb1382c4c7abd12a0
vfg7 <vfg@cin.ufpe.br>
2025-03-15 03:00:20 -0300

    gogo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // Importação do arquivo CSS
 import { BrowserRouter } from "react-router";
 
 import App from './App';
